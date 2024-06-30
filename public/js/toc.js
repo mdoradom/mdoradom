5bba2db4dd7ceb29d2743791cf32731d9e84007f
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2024-06-30 17:01:15 +0200

    added education

--- public/js/toc.js
+++ public/js/toc.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const toc = document.getElementById('toc');
-    const headers = document.querySelectorAll('h2, h3, h4, h5, h6');
+    const headers = document.querySelectorAll('h2');
 
     if (headers.length === 0) return;
 
